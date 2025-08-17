65f1f5dfe48a398a52ef598d79db4d45eed94215
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-08-17 14:01:50 -0300

    Secao 3: - 37.Implementando a Table com Angular Material

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { FormsModule } from '@angular/forms'
+import { MatButtonModule } from "@angular/material/button"
 import { MatCardModule } from '@angular/material/card'
 import { MatFormField, MatLabel } from '@angular/material/form-field'
+import { MatIcon } from "@angular/material/icon"
 import { MatInputModule } from '@angular/material/input'
+import { Cliente } from './cliente'
+import { ClienteService } from '../cliente.service'
 
 @Component({
   selector: 'app-cadastro',
@@ -13,11 +17,21 @@ import { MatInputModule } from '@angular/material/input'
     MatCardModule,
     MatFormField,
     MatInputModule,
-    MatLabel
+    MatLabel,
+    MatIcon,
+    MatButtonModule
   ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
 
+  cliente: Cliente = Cliente.newCliente();
+
+  constructor(private service: ClienteService) { }
+
+  salvar() {
+    this.service.salvar(this.cliente);
+    this.cliente = {};
+  }
 }
--- crud-angular-material/src/app/consulta/consulta.component.ts
+++ crud-angular-material/src/app/consulta/consulta.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from "@angular/material/icon";
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { Cliente } from '../cadastro/cliente';
+import { ClienteService } from '../cliente.service';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatTableModule,
+    MatIcon,
+    MatButtonModule
+  ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit {
+
+  listaClientes: Cliente[] = [];
+  colunasTable: string[] = ["id", "nome", "email", "cpf", "dataNascimento"]
+
+  constructor(private service: ClienteService) { }
+
+  ngOnInit() {
+    this.listaClientes = this.service.pesquisarClientes('');
+  }
 
 }
